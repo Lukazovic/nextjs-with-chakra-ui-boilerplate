06ff7d048b9e60c0746309b05a41d593f56aca17
Lukazovic <lucas10.vieira.lv@gmail.com>
2021-05-18 17:40:33 -0300

    Fix: tests converage to ignore not needed files

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,14 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)?'],
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/**/stories.tsx',
+    '!src/pages/**/*.tsx',
+    '!src/styles/**/*.ts(x)?',
+    '!src/**/mock.ts',
+    '!src/utils/test-utils.tsx'
+  ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   snapshotSerializers: ['@emotion/jest/serializer'],
   modulePaths: ['<rootDir>/src/']
