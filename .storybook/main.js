9bba587cb0741085b7978356418bd4f1316199ad
Lukazovic <lucas10.vieira.lv@gmail.com>
2021-06-06 14:10:49 -0300

    Update storybook main to use absolute imports with no funther configs

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,21 +5,12 @@ const toPath = (_path) => path.join(process.cwd(), _path)
 module.exports = {
   stories: ['../src/components/**/stories.tsx'],
   addons: ['@storybook/addon-essentials'],
-  typescript: {
-    reactDocgen: false,
-  },
-  webpackFinal: async (config) => {
-    return {
-      ...config,
-      resolve: {
-        ...config.resolve,
-        alias: {
-          ...config.resolve.alias,
-          "@emotion/core": toPath("node_modules/@emotion/react"),
-          "emotion-theming": toPath("node_modules/@emotion/react"),
-          "components": toPath("src/components"),
-        },
-      },
-    }
+  webpackFinal: (config) => {
+    config.resolve.alias["@emotion/core"] = toPath("node_modules/@emotion/react")
+    config.resolve.alias["emotion-theming"] = toPath("node_modules/@emotion/react")
+
+    config.resolve.modules.push(`${process.cwd()}/src`);
+
+    return config;
   },
 };
